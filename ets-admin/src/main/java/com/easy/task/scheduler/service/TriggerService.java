bf1134491d351107dbb7039627c7642bcc2de7aa
fistao <1258716078@qq.com>
2019-03-19 10:39:02 +0800

    添加触发器接口

---
+++ ets-admin/src/main/java/com/easy/task/scheduler/service/TriggerService.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C), 2011-2019.
+ */
+package com.easy.task.scheduler.service;
+
+/**
+ * impletation of corn expression
+ * @author jony 2019/3/15 - 14:18.
+ */
+public interface TriggerService {
+
+    /**
+     * deal with core expression
+     */
+    void cornTrigger();
+}
---
+++ ets-admin/src/main/java/com/easy/task/scheduler/service/impl/TriggerServiceImpl.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C), 2011-2019.
+ */
+package com.easy.task.scheduler.service.impl;
+
+import com.easy.task.scheduler.service.TriggerService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jony 2019/3/15 - 14:18.
+ */
+@Component
+public class TriggerServiceImpl implements TriggerService {
+
+    @Override
+    public void cornTrigger() {
+
+    }
+}
